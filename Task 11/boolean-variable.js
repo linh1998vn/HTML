78100733ea278e88b951d2e7cb9df51360547a14
linh1998vn <81789760+linh1998vn@users.noreply.github.com>
2021-05-30 23:03:02 +0700

    Task 11

---
+++ Task 11/boolean-variable.js
@@ -0,0 +1,12 @@
+var inStock;
+var shipping;
+inStock = true;
+shipping = false;
+
+var elStock = document.getElementById('stock');
+
+elStock.className = inStock;
+
+var elship = document.getElementById ('shipping');
+
+elShip.className = Shipping;
